01f6875f09e43ed27e5bb8190d8705d5acc2e224
asmcivor <asmcivor@gmail.com>
2026-02-17 20:18:51 -0800

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from datetime import datetime
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+
+@app.get("/time", response_class=HTMLResponse)
+def get_time(request: Request):
+    return templates.TemplateResponse(
+        "partials/time.html",
+        {"request": request, "time": datetime.now().strftime("%H:%M:%S")}
+    )
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Dashboard is a personal dashboard implemented in htmx with Python
+It is meant as a learning opportunity for python doing web dev
+In addition to the code here the followng need to be installed
+pip install fastapi uvicorn jinja2
